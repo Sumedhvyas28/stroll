ec46f2d822e7b742ded2f42bef567ec7bc010db0
Sumedhvyas28 <sumedhvyas10@gmail.com>
2025-05-30 20:57:16 +0530

    added folder structure and some dependecies

---
+++ lib/ui/screens/stroll_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StrollPage extends StatelessWidget {
+  const StrollPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
